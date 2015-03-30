34023d5d91ec3611f893a7848022d91aabdec4a2
reyley <reyley1014@gmail.com>
2015-03-30 23:43:50 +0300

    fixed edge case with URL of /invitation

--- invitation/urls.py
+++ invitation/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns(
     '',
     url(r'^(?P<invite_id>\d+)/(?P<guest_id>\d+)/rsvp/(?P<rsvp>\w+)/?$', views.invitation_input_rsvp, name="rsvp"),
     url(r'^(?P<invite_id>\d+)/$', views.invitation_detail, name="detail"),
+    url(r'$', views.main_error, name="main_error"),
     url(r'.*/', views.main_error, name="main_error"),
 
 )
