467b25a53fbd5c2857ba65e28d68245c971f5fb7
reyley <reyley1014@gmail.com>
2015-04-30 20:31:11 +0300

    Added send button to all of the invitations

--- invitation/html_templates.py
+++ invitation/html_templates.py
@@ -4,10 +4,10 @@ __author__ = 'User'
 initial_english_html = """
     <p>Hi {0},</p>
     <p>We are happy to inform you that you are invited to Avichai Marks and Devora Moskovitz's wedding!</p>
-    <p>Please view the invitation and RSVP.</p>
+    <p>Please click on the image to view the invitation and RSVP.</p>
     <br/>
     <p><a href="{1}">
-    <div style="text-align:center;">
+    <div style="left-margin: 10%;">
     <img src="http://static.itisourwedding.com/devora-dev/static/email/Eng_envelope_with_stamp.jpg"
         alt="Press on image to go to wedding invitation">
     </div>
@@ -24,10 +24,10 @@ initial_hebrew_html = """
     <p style="direction: rtl; text-align: right;">&nbsp;</p>
     <p style="direction: rtl; text-align: right;">שלום {0},</p>
     <p style="direction: rtl; text-align: right;">אנו מתכבדים להזמינכם לחתונתם של אביחי מרקס ודבורה מוסקוביץ!</p>
-    <p style="direction: rtl; text-align: right;">בבקשה תעיינו בהזמנה ותאשרו את הגעתכם&nbsp;</p>
+    <p style="direction: rtl; text-align: right;">בבקשה תלחצו על התמונה כדי לעיין בהזמנה ולאשר את הגעתכם.&nbsp;</p>
     <p style="direction: rtl; text-align: right;">
     <p><a href="{1}">
-    <div style="text-align:center;">
+    <div style="direction: rtl; text-align: right;">
     <img src="http://static.itisourwedding.com/devora-dev/static/email/He_envelope_with_stamp.jpg"
         alt="לחץ כאן כדי להיכנס להזמנה">
     </div>
@@ -36,7 +36,8 @@ initial_hebrew_html = """
     <p style="direction: rtl; text-align: right;">נשמח לראותכם בשמחתינו!</p>
     <p style="direction: rtl; text-align: right;">אביחי ודבורה</p>
     <br/>
-    <p><small>אם הלינק לא נפתח, בבקשה תנסו להעתיק לתוך הדפדפן את הלינק הבא: </small>
+    <p style="direction: rtl; text-align: right;">
+    <small>אם הלינק לא נפתח, בבקשה תנסו להעתיק לתוך הדפדפן את הלינק הבא: </small>
     <small><a href="{1}">{1}</a></small>
     </p>
 """
